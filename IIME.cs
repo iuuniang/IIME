b9738be952400b99b35565eaadacea68dc66f72e
iuuniang <592858548@qq.com>
2021-04-17 22:46:11 +0800

    Create update.txt Update README.md Update update.txt Update update.txt Update update.txt 之前仅作为更新，现在将源码同步上来
    提交源码
    
    删除无用文件
    
    fix: 修复触发时机问题，更改了占位符号
    
    - 更新 IIME 插件，重构输入法控制逻辑
    - 将项目目标框架版本更新至 v4.7.2。将项目目标框架从 `v3.5` 更新至 `v4.7.2`
    
    Update README.md
    
    还是说得详细点吧
    Update README.md
    Update README.md
    fix: 为了保证plgx的正常使用，将C#版本降低到5.0
    
    build: 重新生成

---
+++ IIME.cs
@@ -0,0 +1,287 @@
+﻿using KeePass.Plugins;
+using KeePass.Util;
+using KeePass.Util.Spr;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+
+namespace IIME
+{
+    public sealed class IIMEExt : Plugin
+    {
+        private const string UPDATEURL = "https://raw.githubusercontent.com/iuuniang/IIME/main/update.txt";
+
+        private const string m_strPlaceholderEnglish = "IME:EN";
+        private const string m_strPlaceholderChinese = "IME:CN";
+
+        private IPluginHost m_host = null;
+
+        private bool _imeStatus = false;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            m_host = host;
+            AutoType.FilterCompilePre += this.OnAutoTypeFilterCompilePre;
+            SprEngine.FilterPlaceholderHints.Add(string.Format("{{{0}}}", m_strPlaceholderEnglish));
+            SprEngine.FilterPlaceholderHints.Add(string.Format("{{{0}}}", m_strPlaceholderChinese));
+
+            return true;
+        }
+
+        public override void Terminate()
+        {
+            SprEngine.FilterPlaceholderHints.Remove(string.Format("{{{0}}}", m_strPlaceholderEnglish));
+            SprEngine.FilterPlaceholderHints.Remove(string.Format("{{{0}}}", m_strPlaceholderChinese));
+            AutoType.FilterCompilePre -= this.OnAutoTypeFilterCompilePre;
+
+        }
+
+        public override string UpdateUrl
+        {
+            get { return UPDATEURL; }
+        }
+
+        public override Image SmallIcon
+        {
+            get { return (Image)KeePass.Program.Resources.GetObject("B16x16_KTouch"); }
+        }
+
+        private void OnAutoTypeFilterCompilePre(object sender, AutoTypeEventArgs autoTypeEventArgs)
+        {
+            Regex replacerEnglish = new Regex(string.Format(@"{{{0}(?:@(\d+))?}}", m_strPlaceholderEnglish), RegexOptions.IgnoreCase);
+            Regex replacerChinese = new Regex(string.Format(@"{{{0}(?:@(\d+))?}}", m_strPlaceholderChinese), RegexOptions.IgnoreCase);
+
+            
+            autoTypeEventArgs.Sequence = replacerEnglish.Replace(autoTypeEventArgs.Sequence, match =>
+            {
+                //InputMethodController.SetIMEStatus(0);
+                int vkey = match.Groups[1].Success ? int.Parse(match.Groups[1].Value) : 16;
+                _imeStatus = InputMethodController.GetIMEStatus();
+                if (_imeStatus)
+                {
+                    return string.Format("{{VKEY {0}}}", vkey);
+                }
+                else
+                {
+                    _imeStatus = true;
+                    return String.Empty;
+                }
+            });
+
+            autoTypeEventArgs.Sequence = replacerChinese.Replace(autoTypeEventArgs.Sequence, match =>
+            {
+                // InputMethodController.SetIMEStatus(1);
+                int vkey = match.Groups[1].Success ? int.Parse(match.Groups[1].Value) : 16;
+                if (_imeStatus)
+                {
+                    _imeStatus = false;
+                    return string.Format("{{VKEY {0}}}", vkey);
+                }
+                else
+                {
+                    return String.Empty;
+                }
+                    
+            });
+        }
+
+        public static class InputMethodController
+        {
+            private const uint GW_CHILD = 0x5;
+            private const uint WM_IME_CONTROL = 0x283;
+
+            private const uint IMC_GETCONVERSIONMODE = 0x0001;
+            private const uint IMC_SETCONVERSIONMODE = 0x0002;
+            private const uint IMC_GETOPENSTATUS = 0x0005;
+            private const uint IMC_SETOPENSTATUS = 0x0006;
+            private const uint IME_CMODE_NOCONVERSION = 0x100;
+            private const uint IME_CMODE_LANGUAGE = 0x3;
+
+            [DllImport("user32.dll", SetLastError = true)]
+            private static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
+            [DllImport("Imm32.dll")]
+            private static extern bool ImmSetConversionStatus(IntPtr hIMC, int fdwConversion, int fdwSentence);
+            [DllImport("Imm32.dll")]
+            private static extern IntPtr ImmGetContext(IntPtr hWnd);
+            [DllImport("imm32.dll")]
+            private static extern IntPtr ImmGetDefaultIMEWnd(IntPtr hWnd);
+            [DllImport("Imm32.dll")]
+            private static extern bool ImmReleaseContext(IntPtr hWnd, IntPtr hIMC);
+            [DllImport("user32.dll ", SetLastError = true)]
+            private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr GetKeyboardLayout(uint idThread);
+            [DllImport("imm32.dll")]
+            private static extern bool ImmGetConversionStatus(IntPtr himc, ref int fdwConversion, ref int fdwSentence);
+            [DllImport("user32.dll", SetLastError = true)]
+            private static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr processId);
+            [DllImport("user32.dll")]
+            private static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
+            [DllImport("kernel32.dll")]
+            private static extern uint GetCurrentThreadId();
+            [DllImport("user32.dll")]
+            private static extern IntPtr GetForegroundWindow();
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool GetGUIThreadInfo(uint idThread, ref GUITHREADINFO lpgui);
+
+            [Flags]
+            private enum GuiThreadInfoFlags
+            {
+                GUI_CARETBLINKING = 0x00000001,
+                GUI_INMENUMODE = 0x00000004,
+                GUI_INMOVESIZE = 0x00000002,
+                GUI_POPUPMENUMODE = 0x00000010,
+                GUI_SYSTEMMENUMODE = 0x00000008
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct GUITHREADINFO
+            {
+                public int cbSize;
+                public GuiThreadInfoFlags flags;
+                public IntPtr hwndActive;
+                public IntPtr hwndFocus;
+                public IntPtr hwndCapture;
+                public IntPtr hwndMenuOwner;
+                public IntPtr hwndMoveSize;
+                public IntPtr hwndCaret;
+                public System.Drawing.Rectangle rcCaret;
+            }
+            public static bool SetIMEStatus(uint status)
+            {
+                return SetOpenStatus(status) == IntPtr.Zero;
+            }
+
+            public static bool GetIMEStatus(IntPtr hWnd = default(IntPtr))
+            {
+                if (hWnd == IntPtr.Zero)
+                {
+                    hWnd = GetForegroundWindow();
+                    hWnd = GetFocus(hWnd,true) ?? hWnd;
+                }
+                bool opened = GetOpenStatus(hWnd);
+                int? convMode = GetConversionMode(hWnd);
+                ushort langId = GetCurrentLangIdByHwnd(hWnd);
+
+                if (opened && langId == 0x409) {return false;}
+                if (convMode == null) { return false;}
+                if ((convMode & IME_CMODE_NOCONVERSION) != 0 ) {return false;}
+                return opened && ((convMode & IME_CMODE_LANGUAGE) != 0);
+            }
+            private static IntPtr? SetOpenStatus(uint status,IntPtr hWnd = default(IntPtr))
+            {
+                if (hWnd == IntPtr.Zero)
+                {
+                    hWnd = GetForegroundWindow();
+                }
+                return ImeControl(hWnd, IMC_SETOPENSTATUS, status);
+            }
+    
+            private static ushort GetCurrentLangIdByHwnd(IntPtr hWnd)
+            {
+                if (hWnd == IntPtr.Zero)
+                {
+                    hWnd = GetForegroundWindow();
+                }
+                uint threadId = GetWindowThreadProcessId(hWnd, IntPtr.Zero);
+
+                return (ushort)((uint)GetKeyboardLayout(threadId).ToInt32() & 0xFFFF);
+            }
+
+            private static int? GetConversionMode(IntPtr hWnd)
+            {
+                if (hWnd == IntPtr.Zero)
+                {
+                    hWnd = GetForegroundWindow();
+                }
+                IntPtr? result = ImeControl(hWnd, IMC_GETCONVERSIONMODE);
+                if (result != null)
+                {
+                    return result.Value.ToInt32();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            private static bool GetOpenStatus(IntPtr hWnd)
+            {
+                if (hWnd == IntPtr.Zero)
+                {
+                    hWnd = GetForegroundWindow();
+                }
+                return ImeControl(hWnd, IMC_GETOPENSTATUS) != IntPtr.Zero; //返回值 0:英文 1:中文
+            }
+
+
+            private static IntPtr? ImeControl(IntPtr hWnd = default(IntPtr), uint command = 0,uint data = 0)
+            {
+                if (hWnd == IntPtr.Zero)
+                {
+                    hWnd = GetForegroundWindow();
+                }
+                IntPtr hIMEWnd = ImmGetDefaultIMEWnd(hWnd);
+                if (hIMEWnd != IntPtr.Zero)
+                {
+                    return SendMessage(hIMEWnd, WM_IME_CONTROL,(IntPtr)command, (IntPtr)data);
+                }
+                return null;
+            }
+
+            private static IntPtr? GetFocus(IntPtr hWnd,bool real=false)
+            {
+                if (hWnd == IntPtr.Zero)
+                {
+                    hWnd = GetForegroundWindow();
+                }
+                GUITHREADINFO? guiThreadInfo = GetGuiThreadInfo(hWnd);
+                if (guiThreadInfo != null)
+                {
+                    if (guiThreadInfo.Value.hwndFocus != IntPtr.Zero)
+                        return guiThreadInfo.Value.hwndFocus;
+                    if ((guiThreadInfo.Value.hwndCaret != IntPtr.Zero) && (guiThreadInfo.Value.flags.HasFlag(GuiThreadInfoFlags.GUI_CARETBLINKING)))
+                    {
+                        return guiThreadInfo.Value.hwndCaret;
+                    }
+                }
+                if (real) { return null;}
+                IntPtr leafHwnd = GetLeafWindow(hWnd);
+                return (leafHwnd == IntPtr.Zero || leafHwnd == hWnd) ? hWnd : leafHwnd;
+
+            }
+            private static IntPtr GetLeafWindow(IntPtr hWnd)
+            {
+                if (hWnd == IntPtr.Zero) {return IntPtr.Zero;}
+                IntPtr currentHwnd = hWnd;
+                IntPtr childHwnd;
+
+                while ((childHwnd = GetWindow(currentHwnd, GW_CHILD)) != IntPtr.Zero)
+                {
+                    currentHwnd = childHwnd;
+                }
+                return currentHwnd;
+            }
+
+            private static GUITHREADINFO? GetGuiThreadInfo(IntPtr hWnd)
+            {
+                if (hWnd != IntPtr.Zero)
+                {
+                    uint threadID = GetWindowThreadProcessId(hWnd,IntPtr.Zero);
+                    GUITHREADINFO guiThreadInfo = new GUITHREADINFO();
+                    guiThreadInfo.cbSize = Marshal.SizeOf(guiThreadInfo);
+                    if (GetGUIThreadInfo(threadID, ref guiThreadInfo) == false)
+                    {
+                        return null;
+                    }
+                    return guiThreadInfo;
+                }
+                return null;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# IME_Placeholder
-Input method plugin, providing {IME:ON} and {IME:OFF} placeholders.
+# IIME
+
+KeePass Password Safe 2 的中文输入法切换插件
+
+[说明](#说明) | [更新日志](#更新日志) | [原理](#原理) | [操作步骤](#操作步骤) 
+
+## 说明
+
+使用 KeePass 多年，在自动输入账号密码时，经常被中文状态的输入法所困扰，经常需要手动切换为英文输入法，十分麻烦。为了解决这个问题，开发了此插件，部分代码参考了 [aardio 输入法与键盘状态检测](https://www.aardio.com/zh-cn/doc/library-guide/std/key/imeState.html) 的实现，在此特别感谢其作者 [Jacen He](https://github.com/aardio) 的开源。
+
+## 更新日志
+### [1.1.0] - 2025-3-16
+- 懒惰了挺长一段时间，终于修复了热心朋友反馈的[触发和切换的问题](https://github.com/iuuniang/IIME/issues/4)。
+- 升级了项目目标框架，从 .NET Framework 3.5 到 .NET Framework 4.7.2。
+- 提供PLGX插件。
+- 调整占位符号为 `{IME:CN}` `{IME:EN}`。
+- 优化了输入法切换的逻辑，兼容性待测试。
+
+### [1.0.0] - 2022-7-30
+- 首次发布。
+- 采用较为简单的切换方法，存在部分问题。
+- 占位符号为 `{IME:ON}` `{IME:OFF}`
+
+
+## 原理
+其实就是检测输入法状态，然后后台发送 `SHIFT键` 进行中英文切换。
+AutoType.FilterCompilePre 下返回 `{VKEY 16}` 用来代替 `SHIFT键` 在编译击键序列的时候就会按下该按键。
+
+考虑到也许有的朋友会使用其他输入法，切换中英文状态并不是 `SHIFT键`，因此在占位符号中添加了 **{IME:CN@X} {IME:CN@X}** 的扩展用法，**X** 的取值请参考 [Microsoft文档-Virtual-Key Codes](https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes)，将 **Value** 转为 **十进制** 替换 **X**。暂时仅支持单键。
+
+默认状态使用 **{IME:CN} {IME:CN}** 即可，其VKEY取值为16。
+
+## 操作步骤
+- 安装插件。将下载的 `IIME.PLGX` 文件放入 **KeePass** 插件目录下。
+- 重新启动 **KeePass**程序。
+- *编辑顶级群组—自动输入—替代默认序列为：`{DELAY 100}{CLEARFIELD}{IME:EN}{USERNAME}{TAB}{PASSWORD}{DELAY 100}{ENTER}{IME:CN}`—确定* 即可，其余的子群组都可以继承这个默认序列。
+
+## 备注
+编译PLGX的方法：```.\KeePass.exe --plgx-create D:\IIME --plugx-prereq-os:Windows```
